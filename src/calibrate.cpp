9dd0e7570d7e415aafcd3e334bb7b60e5c1aa638
gvachonwe <102626427+gvachonwe@users.noreply.github.com>
2022-11-10 10:14:05 -0500

    v2.

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -18,9 +18,9 @@ void loop() {
 
   // Find resistance with voltage divider equation
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
-  logR2 = log(R2);
 
   // Find T (k) using Stein-Hart equation
+  logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
 
   // Covert to celcius degrees
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
 
 // Define global variables
-
+int Vo;
+int ResistPin = 0;
+float R1 = 10000;
+float R2, T, Tc;
 
 void setup() {
-  
+  // Start serial monitor + set speed
+  Serial.begin(9600);
 }
 
 void loop() {
+   // Read pin output
+  Vo = analogRead(ResistPin);
+
+  // Find resistance with voltage divider equation
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
   
 }
