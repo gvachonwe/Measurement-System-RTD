b93929cde442d085b05e869074fd3780d046f4c9
gvachonwe <102626427+gvachonwe@users.noreply.github.com>
2022-11-11 19:12:01 -0500

    v3.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 // Define global variables
 int Vo;
+float V = 0.0;
 int ResistPin = 0;
 float R1 = 10000;
 float R2, T, Tc;
@@ -13,9 +14,15 @@ void setup() {
 
 void loop() {
    // Read pin output
-  Vo = analogRead(ResistPin);
+  Vo = analogRead(A0);
 
-  // Find resistance with voltage divider equation
-  R2 = R1 * (1023.0 / (float)Vo - 1.0);
-  
+  // Convert analog signal to voltage
+  V = Vo*(5.0/1023);
+
+  // Print to serial monitor
+  Serial.print("Voltage: "); 
+  Serial.print(V);
+  Serial.println(" V");
+
+  delay(1000);
 }
