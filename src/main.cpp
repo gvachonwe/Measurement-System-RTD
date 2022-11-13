39d2a33a68f41d14324f4e4c139bd0d7adee0f6a
gvachonwe <102626427+gvachonwe@users.noreply.github.com>
2022-11-12 19:27:47 -0500

    v5.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 // Define global variables
 int Vo;
 float V = 0.0;
+float T = 0.0;
+float R1 = 10000.0;
+float R2 = 10000.0;
 
 // Initialize LCD
 LiquidCrystal lcd(0, 1, 8, 9, 10, 11);
@@ -27,16 +30,22 @@ void loop() {
   V = Vo*(5.0/1023);
 
   // Convert voltage to temp
+  T = (131.5*V) - 333.78;
 
   // Print to serial monitor
   //Serial.print("Voltage: "); 
   //Serial.print(V);
   //Serial.println(" V");
 
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,0);
   lcd.print("Voltage: ");
   lcd.print(V);
   lcd.print(" V");
 
-  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("Temp: ");
+  lcd.print(T);
+  lcd.print(" C");
+
+  delay(500);
 }
